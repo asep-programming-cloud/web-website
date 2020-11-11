925801f97f142a0577b77bb5b6f094a0a5ba34ba
Arul Selvam Eric Paulraj <arul-selvam-eric.paulraj@hpe.com>
2020-11-11 13:03:55 +0530

    Completed Level 4 - bcrypt and Salting

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const bParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -25,13 +26,15 @@ app.route('/register')
         res.render('register');
     })
     .post(function (req, res) {
-        const newUser = new User({
-            email: req.body.username,
-            password: md5(req.body.password)
-        });
-        newUser.save(function (err) {
-            if (err) { console.log(err); }
-            else { res.render('secrets') };
+        bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+            const newUser = new User({
+                email: req.body.username,
+                password: hash
+            });
+            newUser.save(function (err) {
+                if (err) { console.log(err); }
+                else { res.render('secrets') };
+            });
         });
     });
 
@@ -41,16 +44,19 @@ app.route('/login')
     })
     .post(function (req, res) {
         const username = req.body.username;
-        const password = md5(req.body.password);
+        const password = req.body.password;
         User.findOne({ email: username }, function (err, foundUser) {
             if (err) { res.send(err); }
             else {
                 if (foundUser) {
-                    if (foundUser.password === password) {
-                        res.render('secrets');
-                    } else {
-                        res.send('Wrong credentials');
-                    }
+                    bcrypt.compare(password, foundUser.password,
+                        function (err, result) {
+                            if (result) {
+                                res.render('secrets');
+                            } else {
+                                res.send('Wrong credentials');
+                            }
+                        });
                 } else {
                     res.send('Wrong credentials');
                 }
