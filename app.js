868b909064a5c768381eaccb1e978ad2d0d99cff
Arul <asep.programming@gmail.com>
2020-11-12 08:30:25 +0530

    Setup of passport

--- app.js
+++ app.js
@@ -3,19 +3,33 @@ const express = require('express');
 const bParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const app = express();
 app.set('view engine', 'ejs');
 app.use(bParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(session({
+    secret: 'ThisisalongSecret',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect('mongodb://localhost:27017/userDB',
     { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set('useCreateIndex', true)
 
 const userSchema = new mongoose.Schema({ email: String, password: String });
+userSchema.plugin(passportLocalMongoose);
+
 const User = mongoose.model('User', userSchema);
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get('/', function (req, res) {
     res.render('home');
@@ -26,16 +40,6 @@ app.route('/register')
         res.render('register');
     })
     .post(function (req, res) {
-        bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-            const newUser = new User({
-                email: req.body.username,
-                password: hash
-            });
-            newUser.save(function (err) {
-                if (err) { console.log(err); }
-                else { res.render('secrets') };
-            });
-        });
     });
 
 app.route('/login')
@@ -43,26 +47,7 @@ app.route('/login')
         res.render('login');
     })
     .post(function (req, res) {
-        const username = req.body.username;
-        const password = req.body.password;
-        User.findOne({ email: username }, function (err, foundUser) {
-            if (err) { res.send(err); }
-            else {
-                if (foundUser) {
-                    bcrypt.compare(password, foundUser.password,
-                        function (err, result) {
-                            if (result) {
-                                res.render('secrets');
-                            } else {
-                                res.send('Wrong credentials');
-                            }
-                        });
-                } else {
-                    res.send('Wrong credentials');
-                }
-            }
-        })
-    })
+    });
 
 app.listen(3000, function () {
     console.log('Server started at post 3000');
